d089f0426c24913d91063153b12fc3f539662bbe
Dan Watson <danwatson00@yahoo.com>
2018-04-30 15:15:51 -0500

    got proj pages styles pretty well

--- js/DOMBuilder.js
+++ js/DOMBuilder.js
@@ -5,15 +5,39 @@ function projectBuilder(item) {
     console.log("projectBuilder Item", item);
     let projectCardDiv = 
             `<div>
-                <h1>${item.title}</h1>
-                <h2>${item.subtitle}</h2>
-                <h3>${item.projectType}/h3>
-                <img src="${item.imageHome}" alt="website sample image" />
-                <p>${item.brief}</p>
-                <a>View the Github Repo</a>
-                <a href="${item.github}"><img src="github-logo.png"/>Github Repo</a>
-                <a href="${item.url}"><img src="github-logo.png"/>Live Site</a>
-                <a href="${item.designBrief}"><img src="github-logo.png"/>Design Case Study</a>
+                <div class="proj-div">
+                    <h1>${item.title}</h1><h2>${item.subtitle}</h1>
+                    <h2>${item.projectType}</h2>
+                </div>
+                <div class="proj-div">
+                    <h3>About This Project</h3>
+                    <p>${item.brief}</p>
+                </div>
+                <div class="proj-div">
+                    <h3>Tech</h3>
+                    <p>${item.tech}</p>
+                </div>
+                <div class="proj-div">
+                    <h3>Screenshots</h3>
+                    <img src="${item.imageHome}" class="project-img" alt="website sample image" />
+                </div>
+                <div class="proj-div">
+                    <h3>Project Links</h3>
+                    <div class="proj-links">
+                        <div class="proj-link-obj">
+                            <a href="${item.github}" class="proj-link" target="_blank"><img src="/img/github.png" class="proj-link-icon"/></a>
+                            <a  href="${item.github}">Github Repo</a>
+                        </div>
+                        <div class="proj-link-obj">
+                            <a href="${item.url}" target="_blank"><img src="/img/internet.png" class="proj-link-icon"/></a>
+                            <a  href="${item.url}">Live Site</a>
+                        </div>
+                        <div class="proj-link-obj">
+                            <a href="${item.designBrief}" target="_blank"><img src="/img/briefcase.png" class="proj-link-icon"/></a>
+                            <a  href="${item.designBrief}">Design Case Study</a>
+                        </div>
+                    </div>
+                </div>
             </div>`;
 
         $("main").html(projectCardDiv);
